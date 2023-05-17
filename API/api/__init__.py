be8b713d5a207c8d8bb18649a69498279180490c
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2023-05-17 17:50:08 +0600

    make rest api for firevbase

--- API/api/__init__.py
+++ API/api/__init__.py
@@ -3,3 +3,12 @@ from firebase_admin import credentials, initialize_app
 
 cred = credentials.Certificate("api/Key.json")
 default_app = initialize_app(cred)
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '1234monirabc'
+
+    from .userApi import userApi
+
+    app.register_blueprint(userApi, url_prefix  = '/user')
+    return app
---
+++ API/api/userApi.py
@@ -0,0 +1,31 @@
+import uuid
+from flask import Blueprint, request, jsonify
+from firebase_admin import firestore
+
+db = firestore.client()
+user_ref = db.collection('users_2')
+
+userApi = Blueprint('userApi', __name__)
+
+@userApi.route('/add', methods = ['POST'])
+def create():
+    try:
+        id = uuid.uuid4()
+        user_ref.document(id.hex).set(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f'An Error occured: {e}'
+    
+@userApi.route('/list', methods = ['GET'])
+def read():
+    try:
+        all_user = [doc.to_dict() for doc in user_ref.stream()]
+        return jsonify(all_user), 200
+    except Exception as e:
+        return f'An Error occured: {e}'
+    
+
+# @userApi.route('/update', methods = ['POST'])
+# def update():
+#     try:
+        
