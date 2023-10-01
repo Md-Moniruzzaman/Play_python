3c4360f6ea97ea7ef757b4bec82569018278d763
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2023-10-01 11:14:18 +0600

    nested condition

--- problem_solving_code/nested_condition(26 day hackerRank).py
+++ problem_solving_code/nested_condition(26 day hackerRank).py
@@ -1,5 +1,14 @@
 
 d1, m1, y1 = map(int,input().split())
 d2, m2, y2 = map(int,input().split())
-print(d1, m1, y1)
+fine = 0
+if m2 == m1 and y2== y1 and d1>d2:
+    fine = 15*(d1-d2) 
+elif m1>m2 and y2 == y1:
+    fine = 500* (m1-m2)
+elif y1>y2:
+    fine = 10000
+
+print(fine)
+
     
