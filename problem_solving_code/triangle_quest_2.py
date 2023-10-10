961b56df75991a233f9fbf5af7cbeec037ba548f
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2023-10-10 16:16:50 +0600

    problem solved but according to hacker rank only one print statement is allowed and only two line of code is allowed. Ok lets do it.

--- problem_solving_code/triangle_quest_2.py
+++ problem_solving_code/triangle_quest_2.py
@@ -1,9 +1,11 @@
-n =  5
+n =  int(input())
 for i in range(1, n+1):
     # l = str(i for i in range(n+1))
-    for j in range(1,i+1):
-        print(j,end='')
-    for k in range(i-1,0,-1):
-        print(k, end='')
+    # for j in range(1,i+1):
+    #     print(j,end='')
+    # for k in range(i-1,0,-1):
+    #     print(k, end='')
+    # print('')
 
-    print('')
+    # print((pow(10,i)//9)**2)
+    print(int(ascii(1)*i)**2)
