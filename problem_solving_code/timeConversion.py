9115ad9e75c561596bf5490a7d2cd9b5498ed3da
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2023-10-21 22:59:27 +0600

    optimized code

--- problem_solving_code/timeConversion.py
+++ problem_solving_code/timeConversion.py
@@ -15,18 +15,32 @@ import sys
 
 def timeConversion(s):
     # Write your code here
-    med = s[-2:]
-    if med == "PM":
-        hh = int(s[0:2])
-        if hh != 12:
-            hh+=12
-        s = str(hh) + s[2:8]
-    elif med == 'AM':
-        hh = int(s[0:2])
-        if hh == 12:
-            s = '00' + s[2:8]
-        else:
-            s = s[:8]
+    # med = s[-2:]
+    # if med == "PM":
+    #     hh = int(s[0:2])
+    #     if hh != 12:
+    #         hh+=12
+    #     s = str(hh) + s[2:8]
+    # elif med == 'AM':
+    #     hh = int(s[0:2])
+    #     if hh == 12:
+    #         s = '00' + s[2:8]
+    #     else:
+    #         s = s[:8]
+
+    new_s = s
+    if 'AM' in new_s and '12' in new_s[0:2]:
+        new_s = new_s.replace(new_s[:2], '00')
+        print(new_s)
+        return new_s[:-2]
+    if 'PM' in new_s and '12' in new_s[:2]:
+        return s[:-2]
+    if 'PM' in new_s:
+        hh = str(int(new_s[:2] + 12))
+        new_s = new_s.replace(new_s[:2], hh)
+        return new_s[:-2]
+    else:
+        return s[:-2]
 
     return s
 
