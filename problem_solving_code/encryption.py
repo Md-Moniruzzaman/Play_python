4df5a2b43190015b23a7ce58a01e9d245aace071
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2023-10-23 09:38:34 +0600

    Text encription upgrade

--- problem_solving_code/encryption.py
+++ problem_solving_code/encryption.py
@@ -8,23 +8,16 @@ def encryption(s):
     #write your code here
     s = s.replace(' ','')
     row = math.floor(math.sqrt(len(s)))
-    print(row)
     col = math.ceil(math.sqrt(len(s)))
-    print(col)
     row = row if row*col >=len(s) else row+1
-    print(col)
     s_array = []
-    enc_arry = []
     for i in range(0,len(s),col):
         s_array.append(s[i:i+col])
-    print(s_array)
     st = ''
     for i in range(col):
         for str in s_array:
             if len(str)>=i+1:
                 st += str[i] 
-
-        # enc_arry.append(st)
         st+=' '
 
     return st
