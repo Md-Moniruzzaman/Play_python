9ba82d0c7d52222083795d3431daa012fdc53e59
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2023-10-25 15:13:15 +0600

    How many chocolate bar can  buy

---
+++ problem_solving_code/chocolate_bar.py
@@ -0,0 +1,48 @@
+
+
+#
+# Complete the 'chocolateFeast' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER c
+#  3. INTEGER m
+#
+
+def chocolateFeast(n, c, m):
+    # Write your code here
+    bar = n//c
+    wrp = n//c
+    print(f'bar: {bar}')
+    while wrp >=m:
+        new_bar = wrp//m
+        bar+=new_bar
+        wrp += new_bar - new_bar*m
+    return bar
+    
+        
+
+if __name__ == '__main__':
+    
+
+    t = 1
+
+    for t_itr in range(t):
+        # first_multiple_input = input().rstrip().split()
+
+        n = 46860
+
+        c = 28028
+
+        m = 155
+        # n = int(first_multiple_input[0])
+
+        # c = int(first_multiple_input[1])
+
+        # m = int(first_multiple_input[2])
+
+        result = chocolateFeast(n, c, m)
+        print(f'result: {result}')
+
+      
