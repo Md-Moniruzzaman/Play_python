27a118f5a5b36800ed2d052d6c42f9b15b32a2a9
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2023-11-07 14:40:36 +0600

    python question

---
+++ problem_solving_code/find_question_ans.py
@@ -0,0 +1,57 @@
+def f(x):
+    return x+1
+def g(x):
+    return x*2
+def h(x):
+    return f(g(x))
+
+# print(h(2))
+
+# 2. code of array
+
+def func(arr: list):
+    gen = (each for each in arr  if len(arr)>=3)
+    arr = ['Joe', 'Mary']
+    return list(gen)
+
+arr = ['Joe', ' Peter', 'Paul']
+# print(func(arr))
+# Output is []
+
+# 3. code compiling
+
+# c = d// uncomment this
+d = 'Python'
+# print(c)
+
+# Output is NmaeError
+
+# 4. type conversion
+
+a = '1_111'
+# print(float(a))
+
+# output is 1111.0
+
+# 5. What is the output and why?
+
+class Myclass:
+    def __init__(self, my_var) -> None:
+        self.my_var = my_var
+
+    def instance_method(self):
+        self.my_var = 30
+        return f'{self.my_var}'
+
+obj = Myclass(10)
+
+obj.instance_method()
+
+print(obj.my_var)
+# Output is 30
+
+# 6. list remove item when have duplicate value
+g = [1,2,3,2,5]
+g.remove(2)
+# print(g)
+# output [1,3,2,5]
---
+++ problem_solving_code/the_grid_search.py
@@ -0,0 +1,16 @@
+# def gridSearch(G, P):
+#     # Write your code here
+
+# if __name__=='__main__':
+#     t = int(input().strip())
+
+#     for t_itr in range(t):
+#         first_multiple_input = input().rstrip().split()
+#         R = first_multiple_input[0]
+
+#         C = first_multiple_input[1]
+
+#         G = []
+
+#         for _ in range(R):
+
