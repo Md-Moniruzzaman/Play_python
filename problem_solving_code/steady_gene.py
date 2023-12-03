6f6c0b768cf2b7fceb42ab012c438365a4682276
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2023-12-03 13:01:24 +0600

    steady gene done

--- problem_solving_code/steady_gene.py
+++ problem_solving_code/steady_gene.py
@@ -16,6 +16,7 @@ import sys
 def extra_available(all_neo, max_neo_count):
     for count in all_neo.values():
         if count>max_neo_count:
+           
             return True
     return False
 
@@ -37,6 +38,22 @@ def steadyGene(gene):
     if not extra_available(all_neoc, max_neocl_count):
         return 0
 
+    min_sub_count = float("inf")
+    left_itt = 0
+    right_itt = 0
+    while right_itt<gene_lenth:
+        while right_itt<gene_lenth and extra_available(all_neoc, max_neocl_count):
+            s_gene = gene[right_itt]
+            all_neoc[s_gene]-=1
+            right_itt+=1
+
+        while left_itt<gene_lenth and not extra_available(all_neoc, max_neocl_count):
+            s_gene = gene[left_itt]
+            all_neoc[s_gene]+=1
+            left_itt+=1
+        min_sub_count = min(min_sub_count, right_itt-left_itt+1)
+    
+    return min_sub_count
 
 if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
