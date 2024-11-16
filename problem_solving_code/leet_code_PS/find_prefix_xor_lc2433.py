aaa8b26633306818ea6af78a0343aa8af66f7def
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2024-11-16 21:21:37 +0600

    Another problem solved whigh is find prefix xor of Array

---
+++ problem_solving_code/leet_code_PS/find_prefix_xor_lc2433.py
@@ -0,0 +1,8 @@
+def findArray(pref: list[int])->list[int]:
+    res = []
+    res.append(pref[0])
+    for i in range(len(pref)-1):
+        res.append(pref[i] ^ pref[i+1])
+    return res
+
+print(findArray([5,2,0,3,1]))
