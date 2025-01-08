1711f45fe1c646a1013859906858378e927167af
Moniruzzaman <ammoniruzzaman@gmail.com>
2025-01-08 11:57:16 +0600

    removed src/web2py file

--- src_code/web2py/applications/smsgateway/models/common_cid.py
+++
@@ -1,5 +0,0 @@
-import datetime
-import urllib
-db = DAL('mysql://root:@localhost/smsgateway', decode_credentials=True)
-mreporting_http_pass='abC321'
-date_fixed=datetime.datetime.now()+datetime.timedelta(hours=6)
