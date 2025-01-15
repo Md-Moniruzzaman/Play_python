618aacc58111b1fb999208457c2c46ed07221bdf
Moniruzzaman <ammoniruzzaman@gmail.com>
2025-01-15 17:08:27 +0600

    Solved and accepted using new algorithm but execution time 89ms

--- problem_solving_code/leet_code_PS/2270.py
+++ problem_solving_code/leet_code_PS/2270.py
@@ -5,11 +5,11 @@
 class Solution:
     def waysToSplitArray(self, nums: list[int]) -> int:
         total_sum = sum(nums)
+        pre = [0]
         cnt = 0
-        for i in range(1, len(nums)):
-            print(nums[:i], nums[i:])
-            val = sum(nums[:i])
-            if val >= total_sum - val:
+        for i in range(len(nums)-1):
+            pre.append(pre[-1] + nums[i])
+            if pre[-1] >= total_sum - pre[-1]:
                 cnt += 1
         return cnt
         
