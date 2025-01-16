08fd4c58f96ed6956969607d973d3b9e4063a348
Moniruzzaman <ammoniruzzaman@gmail.com>
2025-01-16 22:30:26 +0600

    Solved with this algorithim but execution time so high

--- problem_solving_code/leet_code_PS/1930.py
+++ problem_solving_code/leet_code_PS/1930.py
@@ -2,17 +2,24 @@
     Unique Length-3 Palindromic Subsequences
 '''
 
+from collections import Counter
+
+
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        list1 = []
-        for i in range(len(s)):
-            for j in range(i+1, len(s)):
-                for k in range(j+1, len(s)):
-                    if s[i] == s[k]:
-                        val = s[i] + s[j] + s[k]
-                        list1.append(val) if val not in list1 else None
-        return len(list1)
+        res = set()
+        left_char = set()
+        right_char = Counter(s)
+        for m in s:
+            right_char[m] -= 1
+            for c in left_char:
+                if right_char[c] > 0:
+                    res.add(c + m + c)
+            left_char.add(m)
+        return len(res)
+        
 s = Solution()
 print(s.countPalindromicSubsequence("aabca")) # 3
 print(s.countPalindromicSubsequence("bbcbaba")) # 4
 print(s.countPalindromicSubsequence("adc")) # 0
+print(s.countPalindromicSubsequence("aabcdcagf")) # 5
