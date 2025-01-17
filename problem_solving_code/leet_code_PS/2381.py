0078e2831407f586537baa0484b1faeb979fdaec
Moniruzzaman <ammoniruzzaman@gmail.com>
2025-01-17 11:05:35 +0600

    Solved using this algorithm and time limit exceed

--- problem_solving_code/leet_code_PS/2381.py
+++ problem_solving_code/leet_code_PS/2381.py
@@ -3,40 +3,18 @@
 '''
 
 class Solution:
-    def shiftChar(self, c: str, shift: list[int])->str:
-        abc = "abcdefghijklmnopqrstuvwxyz"
-        if shift[-1] == 1:
-            for c in s[shift[0]:shift[1]]:
-                if c == 'z':
-                    s[s.index(c)] = 'a'
-                else:
-                    s[s.index(c)] = abc[abc.index(c)+1]
-        else:
-            for c in s[shift[0]:shift[1]]:
-                if c == 'a':
-                    s[s.index(c)] = 'z'
-                else:
-                    s[s.index(c)] = abc[abc.index(c)-1]
-        return s
-                
-    
-    
     def shiftingLetters(self, s: str, shifts: list[list[int]]) -> str:
         abc = "abcdefghijklmnopqrstuvwxyz"
         a = list(s)
         for shift in shifts:
-            if shift[-1] == 1:
-                for i, c in enumerate(a[shift[0]:shift[1]+1]):
-                    if c == 'z':
-                        a[shift[0]+i] = 'a'
-                    else:
-                        a[shift[0]+i] = abc[abc.index(c)+1]
-            else:
-                for i, c in enumerate(a[shift[0]:shift[1]+1]):
-                    if c == 'a':
-                        a[shift[0]+i] = 'z'
-                    else:
-                        a[shift[0]+i] = abc[abc.index(c)-1]
+            start, end, direction = shift
+            if direction == 1:  # Shift forward
+                for i in range(start, end + 1):
+                    a[i] = chr((ord(a[i]) - ord('a') + 1) % 26 + ord('a'))
+            else:  # Shift backward
+                for i in range(start, end + 1):
+                    a[i] = chr((ord(a[i]) - ord('a') - 1) % 26 + ord('a'))
+        
         return ''.join(a)
     
 s = Solution()
