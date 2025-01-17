4f0ace3a027395af155a7d1295eae04741f3ea57
Moniruzzaman <ammoniruzzaman@gmail.com>
2025-01-17 11:32:31 +0600

    Solved using this algorithm time limit 55ms

--- problem_solving_code/leet_code_PS/2381.py
+++ problem_solving_code/leet_code_PS/2381.py
@@ -4,18 +4,21 @@
 
 class Solution:
     def shiftingLetters(self, s: str, shifts: list[list[int]]) -> str:
-        abc = "abcdefghijklmnopqrstuvwxyz"
-        a = list(s)
+        prefix_diff = [0] * (len(s)+1)
         for shift in shifts:
             start, end, direction = shift
-            if direction == 1:  # Shift forward
-                for i in range(start, end + 1):
-                    a[i] = chr((ord(a[i]) - ord('a') + 1) % 26 + ord('a'))
-            else:  # Shift backward
-                for i in range(start, end + 1):
-                    a[i] = chr((ord(a[i]) - ord('a') - 1) % 26 + ord('a'))
+            prefix_diff[end+1] += 1 if direction else -1
+            prefix_diff[start] += -1 if direction else 1
         
-        return ''.join(a)
+        diff = 0
+        res = [ord(c) - ord('a') for c in s]
+        for i in reversed(range(len(prefix_diff))):
+            diff += prefix_diff[i]
+            res[i-1] = (res[i-1] + diff) % 26
+            
+        s = [chr(c + ord('a')) for c in res]
+        
+        return ''.join(s)
     
 s = Solution()
 print(s.shiftingLetters("abc", [[0,1,0],[1,2,1],[0,2,1]])) # ace
