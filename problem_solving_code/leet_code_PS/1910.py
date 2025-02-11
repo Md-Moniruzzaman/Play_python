455d4c11d5c4439925929643c660390d643d6b95
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2025-02-11 23:00:01 +0600

    Remove occurences from String

--- problem_solving_code/leet_code_PS/1910.py
+++ problem_solving_code/leet_code_PS/1910.py
@@ -14,7 +14,7 @@ def removeOccurrences(s: str, part: str) -> str:
     #     index = s.find(part)
     #     if index == -1:
     #         break
-    #     del s_list[index:index++len(part)]
+    #     del s_list[index:index+len(part)]
     #     s = ''.join(s_list)
     while part in s:
         s = s.replace(part, '', 1)
