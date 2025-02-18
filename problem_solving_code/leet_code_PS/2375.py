7b355a8627d276e176229865779c1fb575125385
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2025-02-18 22:02:04 +0600

    Smallest number algorithim-> stack, gready

---
+++ problem_solving_code/leet_code_PS/2375.py
@@ -0,0 +1,11 @@
+
+def smallestNumber(pattern: str) -> str:
+    res, stack = [], []
+    
+    for i in range(len(pattern)+1):
+        stack.append(i+1)
+        
+        while stack and (i==len(pattern) or pattern[i] == "I"):
+            res.append(str(stack.pop()))
+    return ''.join(res)
+print(smallestNumber("IIIDIDDI"))
--- problem_solving_code/print_to_ip_printer.py
+++ problem_solving_code/print_to_ip_printer.py
@@ -2,10 +2,26 @@ from escpos.printer import Network
 
 def print_receipt():
     try:
-        printer = Network("192.168.1.100", 9100)  # Replace with your printer's IP
-        printer.text("Hello, Printer!\n")
-        printer.text("This is a test print.\n")
+        printer = Network("10.168.122.179", 9100)  # Replace with your printer's IP
+
+        # Print image at the top (Use a BMP file instead of ICO)
+        # printer.image(r"D:\Tfl_data_hub\dist\PH_logo.png")  # Replace with your BMP image path
+
+        # Set alignment to center
+        printer.set(align='center', double_width=True,double_height=True, bold= True)
+
+        # Print text in the center
+        printer.text("KFC\n")
+        printer.text("Dine In\n")
+        printer.text("Invoice No. 4456\n")
+        printer.text("Thank you!\n")
+
+        # Add extra space before cutting
+        printer.text("\n" * 3)  # Add 5 blank lines before cutting
+
+        # Cut the paper
         printer.cut()
+
         print("Printed successfully.")
     except Exception as e:
         print(f"Printing failed: {e}")
