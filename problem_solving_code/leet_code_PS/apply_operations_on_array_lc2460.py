1bf179e45041c57797a4e9e1117ef7ce497da506
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2025-03-02 00:46:03 +0600

    olp problem to optimized reduce memory

--- problem_solving_code/leet_code_PS/apply_operations_on_array_lc2460.py
+++ problem_solving_code/leet_code_PS/apply_operations_on_array_lc2460.py
@@ -1,8 +1,25 @@
 def applyOprations(nums: list[int])->list[int]:
+    res = []
+    k = 0
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i + 1]:
+            nums[i] *= 2
+            nums[i + 1] = 0
+        if nums[i] != 0:
+            res.append(nums[i])
+        else:
+            # print('k:',k)
+            k+=1
+    
+    res.append(nums[-1])
+    res+= [0]*k
+    return res
+    
     for i in range(0,len(nums)-1):
         if nums[i] == nums[i+1]:
             nums[i] = nums[i] * 2
             nums[i+1] = 0
+    
     
     j = 0
     for i in range(len(nums)):
@@ -14,5 +31,7 @@ def applyOprations(nums: list[int])->list[int]:
             
     return nums
     
-    
-applyOprations([1,1,2,1,1])
+print(applyOprations([1,2,2,1,1, 0]))
+print(applyOprations([1,0,2,2,0,3,4])) # [1,2,2,3,4,0,0]
+print(applyOprations([1,2,3,4])) # [1,2,3,4]
+print(applyOprations([0,0,0,1,2,3,4])) # [1,2,3,4,0,0,0]
