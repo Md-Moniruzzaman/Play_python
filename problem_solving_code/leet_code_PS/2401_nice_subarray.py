73dd5e0a5ff382f8a4a3c83c455e7b2668b99ccd
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2025-03-19 12:33:44 +0600

    Nice subarray

---
+++ problem_solving_code/leet_code_PS/2401_nice_subarray.py
@@ -0,0 +1,31 @@
+'''You are given an array nums consisting of positive integers.
+
+We call a subarray of nums nice if the bitwise AND of every pair of elements that are in different positions in the subarray is equal to 0.
+
+Return the length of the longest nice subarray.
+
+A subarray is a contiguous part of an array.
+
+Note that subarrays of length 1 are always considered nice.'''
+
+
+from typing import List
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        res = 0
+        cur = 0
+        l = 0
+        for r in range(len(nums)):
+            while cur & nums[r]:
+                cur ^= nums[l]
+                l+=1
+            
+            res = max(res, r - l + 1)
+            cur = cur | nums[r]
+            
+
+        return res 
+    
+# Examle
+s = Solution()
+print(s.longestNiceSubarray([1,3,8,48,10])) #3
