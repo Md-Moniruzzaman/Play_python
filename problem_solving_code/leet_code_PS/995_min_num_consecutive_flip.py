267f78a2c81a9d885361a4632330ad8030024187
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2025-03-19 13:51:29 +0600

    Minumum number of conscutive flip After optimization

--- problem_solving_code/leet_code_PS/995_min_num_consecutive_flip.py
+++ problem_solving_code/leet_code_PS/995_min_num_consecutive_flip.py
@@ -2,22 +2,41 @@
 
 class Solution:
     def minKBitFlips(self, nums: list[int], k: int) -> int:
-        def flip(nums, i):
-            nums[i] = 0 if nums[i] else 1
-        count = 0
-        for i in range(len(nums)-(k-1)):
-            # print(nums[i])
-            if nums[i] == 0:
-                # print(i+k)
-                for j in range(i,i+k):
-                    flip(nums, j)
-                count+=1
-        # print(nums)
-        for i in range(1, k+1):
-            if nums[-i] == 0:
-                return -1
+        # def flip(nums, i):
+        #     nums[i] = 0 if nums[i] else 1
+        # count = 0
+        # for i in range(len(nums)-(k-1)):
+        #     # print(nums[i])
+        #     if nums[i] == 0:
+        #         # print(i+k)
+        #         for j in range(i,i+k):
+        #             flip(nums, j)
+        #         count+=1
+        # # print(nums)
+        # for i in range(1, k+1):
+        #     if nums[-i] == 0:
+        #         return -1
         
-        return count
+        # return count
+        
+        # After optimization 
+        n = len(nums)
+        flip, res = 0, 0
+        is_flipped = [0] * n  # Track flips at each index
+
+        for i in range(n):
+            if i >= k:
+                flip ^= is_flipped[i - k]  # Remove the effect of flips that are out of range
+
+            if nums[i] == flip:  # If flip is even, original nums[i] should be 1; if odd, it should be 0
+                if i + k > n:
+                    return -1  # Not enough elements left to flip
+
+                is_flipped[i] = 1  # Mark a flip at this position
+                flip ^= 1  # Toggle the flip state
+                res += 1  # Increase the flip count
+
+        return res
     
 # Example 
 s = Solution()
