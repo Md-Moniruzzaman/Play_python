bd7602080e0cd9b7fadc220976f15b419a24ebe0
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2025-04-03 21:03:11 +0600

    solve daily question and bit maipilation of duplicate array

--- problem_solving_code/leet_code_PS/90_subsets_ii.py
+++ problem_solving_code/leet_code_PS/90_subsets_ii.py
@@ -4,13 +4,25 @@ class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort() # Sort the array to handle duplicates
         n = len(nums)
-        res = set()
-        # Generate all subsets using bitmasking
-        for mask in range(1<< n):
-            subset = tuple(nums[i] for i in range(n) if (mask & (1 << i))) # Include elements where bit is set
-            res.add(subset)  # Use a set to avoid duplicates
-        # Convert set to list of lists
-        return list(map(list, res))
+        res = []
+        # # Generate all subsets using bitmasking
+        # for mask in range(1<< n):
+        #     subset = tuple(nums[i] for i in range(n) if (mask & (1 << i))) # Include elements where bit is set
+        #     res.add(subset)  # Use a set to avoid duplicates
+        # # Convert set to list of lists
+        # return list(map(list, res))
+        def backtrack(start: int, subset: List[int]):
+            res.append(subset.copy())
+            for i in range(start, n):
+                if i>start and nums[i] == nums[i-1]:
+                    continue  # Skip duplicates
+                subset.append(nums[i])
+                backtrack(i + 1, subset)
+                subset.pop()
+                
+        # Start backtracking from index 0 with an empty subset
+        backtrack(0, [])
+        return res
 
 # Example usage
 s = Solution()
